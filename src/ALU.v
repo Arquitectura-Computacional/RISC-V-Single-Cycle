ac7a8293c0478899afc1eb2b0fb27cec46b97a8c
Alex <57267175+MiriamMalta@users.noreply.github.com>
2022-06-23 22:02:38 -0500

    Added LUI operation

--- src/ALU.v
+++ src/ALU.v
@@ -34,8 +34,8 @@ always @ (A_i or B_i or ALU_Operation_i)
 	begin
 		case (ALU_Operation_i)
 		
-			ADD:
-				ALU_Result_o = A_i + B_i;
+			LUI: //lui
+            			ALU_Result_o = {B_i[19:0],12'b0};
 
 		
 			default:
