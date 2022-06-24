e65c4577a226979c0ba9013571c08249e28d419e
Alex <57267175+MiriamMalta@users.noreply.github.com>
2022-06-23 22:06:03 -0500

    Added LUI param

--- src/ALU_Control.v
+++ src/ALU_Control.v
@@ -23,8 +23,7 @@ module ALU_Control
 
 );
 
-localparam R_Type_ADD	= 7'b0_000_000;
-localparam I_Type_ADDI 	= 7'bx_001_000;
+localparam U_Type_LUI         = 7'bx_010_xxx;
 
 reg [3:0] alu_control_values;
 wire [6:0] selector;
